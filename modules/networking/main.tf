5435146a0aaa08f32af83a3063229ba8feeac611
Rkeram <65199967+Rkeram@users.noreply.github.com>
2023-11-21 20:17:50 +0530

    Update main.tf

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -21,7 +21,7 @@ resource "aws_security_group" "allow_ssh_pub" {
     description = "SSH from the internet"
     from_port   = 0
     to_port     = 65535
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
