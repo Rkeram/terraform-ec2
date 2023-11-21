2930cc940f175f6b7298d96bee4ac5b615c7e5fd
Rkeram <65199967+Rkeram@users.noreply.github.com>
2023-11-21 20:22:48 +0530

    Update main.tf

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -20,14 +20,14 @@ resource "aws_security_group" "allow_ssh_pub" {
   ingress {
     description = "SSH from the internet"
     from_port   = 0
-    to_port     = 65535
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
-    to_port     = 65535
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
